beed4a802aaf1c52b7a68ef9f62897941d2e51e1
shawn220a <shawn.tschoepe@icloud.com>
2020-01-09 10:14:06 -0600

    Clean up code

--- js/questions.js
+++ js/questions.js
@@ -1,22 +1,21 @@
-
 var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var response = JSON.parse(xhttp.responseText);
-      var questions = response.questions;
-      console.log(questions);
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var response = JSON.parse(xhttp.responseText);
+    var questions = response.questions;
+    console.log(questions);
 
-      document.querySelector("#titleHTML").innerHTML = questions[0].title;
-      
-      var output = '';
-      for(var i = 0;i < 3;i++){
-        output += '<button>'+questions[0].choices[i]+'</button>' + '</br>';
-      }
-      
-      document.querySelector(".choices").innerHTML = output;
-      
+    document.querySelector("#titleHTML").innerHTML = questions[0].title;
+
+    var output = '';
+    for (var i = 0; i < 3; i++) {
+      output += '<button>' + questions[0].choices[i] + '</button>' + '</br>';
     }
-  };
 
-  xhttp.open("GET", "./js/questions.json", true);
-  xhttp.send();
+    document.querySelector(".choices").innerHTML = output;
+
+  }
+};
+
+xhttp.open("GET", "./js/questions.json", true);
+xhttp.send();
