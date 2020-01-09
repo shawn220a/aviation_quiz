8b39195efc54e44464634ae5933451140bdaa111
shawn220a <shawn.tschoepe@icloud.com>
2020-01-08 18:34:08 -0600

    Print question to page

--- js/questions.js
+++ js/questions.js
@@ -1,49 +1,22 @@
-const questions = [
-  {
-    "title": "What makes up a Thunderstorm:",
-    "choices": [
-      "water vapor, unstable lapse rate, upward lifting",
-      "water, lightning, thunder",
-      "clouds, lightning, thunder"
-    ],
-    "answer": "water vapor, unstable lapse rate, upward lifting"
-  },
-  {
-    "title": "When making routine transponder code changes, pilots should avoid inadvertent selection of which code:",
-    "choices": [
-      7200,
-      7000,
-      7500
-    ],
-    "answer": 7500
-  },  
-  {
-    "title": "What does the red line on an airspeed indicator represent:",
-    "choices": [
-      "Maneuvering speed",
-      "Never-exceed speed",
-      "Turbulent or rough-air speed"
-    ],
-    "answer": "Never-exceed speed"
-  },
-  {
-    "title": "The term `angle of attack` is defined as the angle between the:",
-    "choices": [
-      "chord line of the wing and the relative wind",
-      "airplane`s longitudinal axis and that of the air striking the airfoil",
-      "airplane`s center line and the relative wind"
-    ],
-    "answer": "chord line of the wing and the relative wind"
-  },
-  {
-    "title": "If Receiver Autonomous Integrity Monitoring (RAIM) capability is lost in-flight:",
-    "choices": [
-      "the pilot may still rely on GPS derived altitude for vertical information",
-      "the pilot has no assurance of the accuracy of the GPS position",
-      "GPS position is reliable provided at least 3 GPS satellites are available"
-    ],
-    "answer": "the pilot has no assurance of the accuracy of the GPS position"
-  }
-];
 
-console.log(questions);
+var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var response = JSON.parse(xhttp.responseText);
+      var questions = response.questions;
+      console.log(questions);
+
+      document.querySelector("#titleHTML").innerHTML = questions[0].title;
+      
+      var output = '';
+      for(var i = 0;i < 3;i++){
+        output += '<button>'+questions[0].choices[i]+'</button>' + '</br>';
+      }
+      
+      document.querySelector(".choices").innerHTML = output;
+      
+    }
+  };
+
+  xhttp.open("GET", "./js/questions.json", true);
+  xhttp.send();
