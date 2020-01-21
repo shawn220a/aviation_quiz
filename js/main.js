801449139716c090ffba8ad607cbec7215a6b54f
Shawn Tschoepe <54966413+shawn220a@users.noreply.github.com>
2020-01-20 22:09:30 -0600

    Fixed link

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const scoresBtn = document.querySelector("#highscores");
 let playerlist = localStorage.getItem('playerList')
 
 function startQuiz() {
-  location.replace("../questions.html");
+  location.replace("questions.html");
 }
 
 function showScores() {
